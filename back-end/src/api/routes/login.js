f989cc3fa0df64ada9a607d7bc888ae44ce662be
rodolfo <rodolfo_mno@hotmail.com>
2022-06-02 17:14:11 -0300

    add: adiciona validacao de login

--- back-end/src/api/routes/login.js
+++ back-end/src/api/routes/login.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const loginController = require('../controller/loginController');
+const loginValidation = require('../../middleware/loginValidation');
 
 const loginRouter = Router();
 
-loginRouter.post('/', loginController.login);
+loginRouter.post('/', loginValidation, loginController.login);
 
 module.exports = loginRouter;
--- back-end/src/api/schemas/login.js
+++ back-end/src/api/schemas/login.js
@@ -1,4 +1,4 @@
-import Joi from 'joi';
+const Joi = require('joi');
 
 const emptyField = 'All fields must be filled';
 
