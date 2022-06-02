e958675093fddd14ffaa762d457475c1700d20fd
Walace <walacewab@gmail.com>
2022-06-02 17:01:55 -0300

    feat: implementa page register

--- front-end/src/pages/register/index.jsx
+++ front-end/src/pages/register/index.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
+  const navigate = useNavigate();
+
+  const handleRegister = () => {
+    navigate("/login");
+  };
+
   return (
     <div>
-      <h1>Register</h1>
+      <form className="container-form">
+        <input
+          data-testid="common_register__input-name"
+          type="text"
+          name="name"
+          id=""
+        />
+        <input
+          data-testid="common_register__input-email"
+          type="email"
+          name="email"
+          id=""
+        />
+        <input
+          data-testid="common_register__input-password"
+          type="password"
+          name="password"
+          id=""
+        />
+        <button
+          data-testid="common_register__button-register"
+          type="button"
+          onClick={handleRegister}
+        >
+          Confirmar
+        </button>
+      </form>
     </div>
   );
 }
