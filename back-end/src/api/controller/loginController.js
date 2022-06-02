4b03c796b33578f92a753e7037dbf39a5e92242c
rodolfo <rodolfo_mno@hotmail.com>
2022-06-02 16:14:43 -0300

    refactor: refatora codigo desnecessario

--- back-end/src/api/controller/loginController.js
+++ back-end/src/api/controller/loginController.js
@@ -3,15 +3,6 @@ const jwt = require('jsonwebtoken');
 const loginService = require('../service/loginService');
 
 const secret = fs.readFileSync('jwt.evaluation.key', { encoding: 'utf8' }).trim();
-const getUserConstroller = async (req, res, _next) => {
-  const { email, password } = req.body;
-
-  const user = await loginService.getUser(email, password);
-
-  if (!user) return res.status(401).json({ message: 'sei la' });
-
-  res.status(200).json(user);
-};
 
 const addUser = async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -53,7 +44,6 @@ const login = async (req, res, next) => {
 };
 
 module.exports = { 
-  getUserConstroller, 
   addUser,
   login,
 };
