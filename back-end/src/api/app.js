017b7585eb75961eb98f7b4120adf78942115a36
Walace <walacewab@gmail.com>
2022-06-03 19:48:22 -0300

    feat: implementa o cors e modifica o .env

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const errorMiddleware = require('../middleware/errorMiddleware');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
