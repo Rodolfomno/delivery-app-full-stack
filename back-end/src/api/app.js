9f4a133c5f17a58c984997936f0b1c0685834a2e
LucianoJunior <lucianojunior4242@gmail.com>
2022-06-03 18:48:56 -0300

    feat: adiciona rota estatica para requisição das imagens

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const errorMiddleware = require('../middleware/errorMiddleware');
+const auth = require('../auth/validadeJWT');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(auth, express.static('public'));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
