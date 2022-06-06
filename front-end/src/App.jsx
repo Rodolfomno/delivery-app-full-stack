e5eb6c33ab80f0b4838b003d079949ad4499ea73
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-06-06 10:17:42 -0300

    feat: cria rota para clients/products

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,14 +2,15 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import React from 'react';
 import Login from './pages/login';
 import Register from './pages/register';
+import Products from './pages/products';
 import './App.css';
-// import rockGlass from './images/rockGlass.svg';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={ <Login /> } />
       <Route path="/register" element={ <Register /> } />
+      <Route path="/customer/products" element={ <Products /> } />
       <Route expec path="/" element={ <Navigate to="/login" /> } />
     </Routes>
   );
