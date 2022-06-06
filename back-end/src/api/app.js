57ba8c7819e791a6b6916542088145c594b1e80f
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-06-06 10:40:18 -0300

    feat: implementa Cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const errorMiddleware = require('../middleware/errorMiddleware');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use(routes);
