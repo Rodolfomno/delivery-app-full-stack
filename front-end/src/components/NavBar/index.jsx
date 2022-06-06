fa5113fcabde560cbc6af9ca0f6ca15d974ceda0
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-06-06 10:29:36 -0300

    feat: add data-testid no componente NavBar

--- front-end/src/components/NavBar/index.jsx
+++ front-end/src/components/NavBar/index.jsx
@@ -5,18 +5,30 @@ function NavBar() {
   return (
     <nav>
       <div className="btn-routes">
-        <button type="button">
+        <button
+          data-testid="customer_products__element-navbar-link-products"
+          type="button"
+        >
           PRODUTOS
         </button>
-        <button type="button">
+        <button
+          data-testid="customer_products__element-navbar-link-orders"
+          type="button"
+        >
           MEUS PEDIDOS
         </button>
       </div>
       <div className="btn-login">
-        <button type="button">
+        <button
+          data-testid="customer_products__element-navbar-user-full-name"
+          type="button"
+        >
           Usuário
         </button>
-        <button type="button">
+        <button
+          data-testid="customer_products__element-navbar-link-logout"
+          type="button"
+        >
           SAIR
         </button>
       </div>
