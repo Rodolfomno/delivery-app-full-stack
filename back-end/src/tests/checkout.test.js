20b56b31289cb04a7bd0640f6383af711b12e4b8
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-06-07 17:28:21 -0300

    test : Caso o vendedor e cliente sejam iguais

--- back-end/src/tests/checkout.test.js
+++ back-end/src/tests/checkout.test.js
@@ -62,7 +62,33 @@ describe('Testes de integração para rota "/checkout"', () => {
       expect(chaiHttpResponse.status).to.be.equal(400);
       expect(chaiHttpResponse.body.message).to.be.equal('"sellerId" must be a number');
     })
-    it('3 - Caso a requisição tenha o formato esperado', async () => {
+    it('3 - Caso o vendedor e cliente sejam iguais', async () => {
+      chaiHttpResponse = await chai.request(app).post('/checkout').send({
+        "userId": 3,
+        "sellerId": 3,
+        "totalPrice": 100,
+        "deliveryAddress": "Rua da Margura",
+        "deliveryNumber": 997811212,
+        "products": [
+          {
+            "productId": 4,
+            "quantity": 6
+          },
+          {
+            "productId": 5,
+            "quantity": 6
+          },
+          {
+            "productId": 9,
+            "quantity": 6
+          }
+        ]
+      });
+
+      expect(chaiHttpResponse.status).to.be.equal(409);
+      expect(chaiHttpResponse.body.message).to.be.equal('Seller and customer cannot be the same');
+    })
+    it('5 - Caso a requisição tenha o formato esperado', async () => {
       before(async () => {
         sinon.stub(Sales, 'create').resolves({ id: 1 });
         sinon.stub(SalesProducts, 'create').resolves();
