9dc4eedf4c42d8357228cdae68230c7d59278c23
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-06-07 17:17:14 -0300

    feat: cria componete CarButton

---
+++ front-end/src/components/CarButton/inde.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './CarButton.css';
+
+function CarButton() {
+  return (
+    <button
+      className="car-btn"
+      type="button"
+    >
+      Ver Carrinho: R$ 300.00
+    </button>
+  );
+}
+
+export default CarButton;
