d968f562a9785b517ebf9ad56ec02a70d6d10153
LucianoJunior <lucianojunior4242@gmail.com>
2022-06-07 18:23:32 -0300

    fix: remove o auth na requisição das imagens

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const errorMiddleware = require('../middleware/errorMiddleware');
-const auth = require('../auth/validadeJWT');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-app.use(auth, express.static('public'));
 app.use(routes);
-app.use(auth, express.static('public'));
+app.use(express.static('public'));
 app.use(errorMiddleware);
 
 module.exports = app;
