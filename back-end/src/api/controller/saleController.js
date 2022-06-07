b70adbf00f7e7cf0d43549e8c3aaa943cbd0700a
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-06-07 20:25:57 -0300

    feat : implementa controller para buscar pedidos pelo id de usuário

--- back-end/src/api/controller/saleController.js
+++ back-end/src/api/controller/saleController.js
@@ -5,7 +5,7 @@ const checkoutSale = async (req, res, next) => {
   const newSale = saleObject(req.body);
   const { products } = req.body;
   try {
-    const response = await saleService(newSale, products);
+    const response = await saleService.create(newSale, products);
 
     if (response.message === 'Seller and customer cannot be the same') {
       return res.status(409).json(response);
@@ -19,4 +19,17 @@ const checkoutSale = async (req, res, next) => {
   }
 };
 
-module.exports = { checkoutSale };
+const getAllSalesByUserId = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const response = await saleService.findAllSalesByUserId(id);
+    if (response.message) return res.status(404).json(response);
+
+    return res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+module.exports = { checkoutSale, getAllSalesByUserId };
