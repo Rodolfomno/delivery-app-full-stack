01f8924dc1a4d7531066d7ae21f6ece88b4251e9
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-06-07 17:21:51 -0300

    feat : implementa tratamento de erro no service e controller da rota "/checkout "

--- back-end/src/api/controller/saleController.js
+++ back-end/src/api/controller/saleController.js
@@ -5,8 +5,14 @@ const checkoutSale = async (req, res, next) => {
   const newSale = saleObject(req.body);
   const { products } = req.body;
   try {
-    const saleIdInserted = await saleService(newSale, products);
-    return res.status(201).json({ saleId: saleIdInserted });
+    const response = await saleService(newSale, products);
+
+    if (response.message === 'Seller and customer cannot be the same') {
+      return res.status(409).json(response);
+    }
+    if (response.message) return res.status(404).json(response);
+
+    return res.status(201).json({ saleId: response });
   } catch (error) {
     console.log(error);
     next(error);
