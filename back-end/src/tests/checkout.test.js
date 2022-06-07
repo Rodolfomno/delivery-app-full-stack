4703072d9a6edb69465027a30e85602ff02c4765
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-06-07 16:43:59 -0300

    test: Caso a requisição não submeta informações da entrega corretamente

---
+++ back-end/src/tests/checkout.test.js
@@ -0,0 +1,40 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const app = require('../api/app');
+const { Sales, SalesProducts } = require('../database/models');
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('Testes de integração para rota "/checkout"', () => {
+  describe('Testa requisição com método POST', () => {
+    it('1 - Caso a requisição não submeta informações da entrega corretamente', async () => {
+      chaiHttpResponse = await chai.request(app).post('/checkout').send({
+        "userId": 3,
+        "sellerId": 2,
+        "totalPrice": 100,
+        "deliveryAddress": "rua",
+        "deliveryNumber": 997811212,
+        "products": [
+          {
+            "productId": 4,
+            "quantity": 6
+          },
+          {
+            "productId": 5,
+            "quantity": 6
+          },
+          {
+            "productId": 9,
+            "quantity": 6
+          }
+        ]
+      });
+
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body.message).to.be.equal('Incomplete address');
+    })
+  })
+})
