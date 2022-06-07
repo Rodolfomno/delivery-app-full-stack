1eab3c34784d52af2b5f26dca60286ab6ff2ed2c
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-06-07 13:53:00 -0300

    fix: export default

--- front-end/src/context/ProductsProvider.js
+++ front-end/src/context/ProductsProvider.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { MyContext } from './MyContext';
+import MyContext from './MyContext';
 
-export function ProductsProvider({children}) {
+function ProductsProvider({ children }) {
   const contextValue = {};
 
   return (
     <MyContext.Provider value={ contextValue }>
       { children }
     </MyContext.Provider>
-  )
+  );
 }
 
 ProductsProvider.propTypes = {
   children: PropTypes.node.isRequired,
-} 
+};
+
+export default ProductsProvider;
