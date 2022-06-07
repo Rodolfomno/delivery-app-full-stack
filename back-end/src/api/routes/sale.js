c339f7d1dde260b6fa00e416478ca137ecccabbb
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-06-06 22:22:47 -0300

    feat : implementa o middleware de validação na rota

--- back-end/src/api/routes/sale.js
+++ back-end/src/api/routes/sale.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
+const saleValidation = require('../../middleware/saleValidation');
 const saleController = require('../controller/saleController');
 
 const saleRouter = Router();
 
-saleRouter.post('/', saleController.checkoutSale);
+saleRouter.post('/', saleValidation, saleController.checkoutSale);
 
 module.exports = saleRouter;
