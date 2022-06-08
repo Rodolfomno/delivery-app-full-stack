fff5a01283269d0e6ce916e9419fd2ab343f6fc3
Walace <walacewab@gmail.com>
2022-06-07 21:56:17 -0300

    fix: remove linhas não utilizadas do arquivo index.jx

--- front-end/src/pages/Orders/index.jsx
+++ front-end/src/pages/Orders/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import NavBar from '../../components/NavBar';
-main-group-7-base-myRequest-front
 import CardOrders from '../../components/CardOrders';
 import './order.css';
-main-group-7-base
 
 function Orders() {
   return (
