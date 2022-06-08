29e6cc7f4748a7cb99670704e854f1965d57b42f
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-06-08 13:46:35 -0300

    refactor: retira useEffect e passa span dentro de button

--- front-end/src/components/CartButton/index.jsx
+++ front-end/src/components/CartButton/index.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import MyContext from '../../context/MyContext';
 
 import './CartButton.css';
 
 function CartButton() {
   const navigate = useNavigate();
+  const { cartTotalValue } = useContext(MyContext);
+  // const [cartTotalValue, setCartTotalValue] = useState(0);
+  // useEffect(() => {
+  //   const cart = JSON.parse(localStorage.getItem('cart')) || [];
+  //   console.log('LS', cart);
+  //   if (cart.length) {
+  //     const totalValue = cart.map((prod) => Number(prod.price) * prod.qtd)
+  //       .reduce((acc, price) => acc + price, 0);
+  //     console.log('totalValueRDC', totalValue);
+  //     setCartTotalValue(totalValue);
+  //     console.log('DEptotalValueRDC', totalValue);
+  //     console.log('useEffect', cart);
+  //   }
+  // }, [countProduct]);
 
   return (
     <button
@@ -13,7 +28,11 @@ function CartButton() {
       type="button"
       onClick={ () => navigate('/customer/checkout') }
     >
-      Ver Carrinho: R$ 300.00
+      <span
+        data-testid="customer_products__checkout-bottom-value"
+      >
+        { `Ver Carrinho: ${cartTotalValue} `}
+      </span>
     </button>
   );
 }
