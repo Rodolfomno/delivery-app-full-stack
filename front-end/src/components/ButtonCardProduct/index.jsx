ff395253da77a27dbe331174e4b1d5be0b30a3de
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-06-08 09:41:16 -0300

    reafactor: logica de add e romeve pelos botoes

--- front-end/src/components/ButtonCardProduct/index.jsx
+++ front-end/src/components/ButtonCardProduct/index.jsx
@@ -2,24 +2,70 @@ import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import './ButtonCardProduct.css';
 
-function ButtonCardProduc({ id }) {
+function ButtonCardProduc({ product }) {
+  // const { id, name, price, urlImage } = product;
   const [countProduct, setCountProduct] = useState(0);
+
+  // const updateLocalStora = (boolean, cart) => {
+  //   if (boolean) {
+  //     const updateQtd = cart.map((prod) => ({ ...prod, qtd: countProduct + 1 }));
+  //     localStorage.setItem('cart', JSON.stringify(updateQtd));
+  //   } else {
+  //     cart.push({ ...product, qtd: countProduct + 1 });
+  //     localStorage.setItem('cart', JSON.stringify(cart));
+  //   }
+  // };
+
+  const removeProduct = () => {
+    if (countProduct !== 0) {
+      const cart = JSON.parse(localStorage.getItem('cart')) || [];
+      setCountProduct(countProduct - 1);
+      const updateQtd = cart.map((prod) => ({ ...prod, qtd: countProduct - 1 }));
+      localStorage.setItem('cart', JSON.stringify(updateQtd));
+    }
+  };
+
+  const addProduct = () => {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    setCountProduct(countProduct + 1);
+    console.log(cart);
+    const ifContained = cart.some((prod) => prod.id === product.id);
+    // updateLocalStora(ifContained, cart);
+    if (ifContained) {
+      const updateQtd = cart.map((prod) => ({ ...prod, qtd: countProduct + 1 }));
+      localStorage.setItem('cart', JSON.stringify(updateQtd));
+    } else {
+      cart.push({ ...product, qtd: countProduct + 1 });
+      localStorage.setItem('cart', JSON.stringify(cart));
+    }
+  };
+
+  const handleCoutProduct = ({ target }) => {
+    const { value } = target;
+    setCountProduct(Number(value));
+  };
   return (
     <div className="btn-rm-add">
       <button
-        data-testid={ `customer_products__button-card-rm-item-${id}` }
+        data-testid={ `customer_products__button-card-rm-item-${product.id}` }
+        className="btn-rm"
         type="button"
-        onClick={ () => setCountProduct(countProduct - 1) }
+        onClick={ removeProduct }
       >
         -
       </button>
-      <span data-testid={ `customer_products__input-card-quantity-${id}` }>
-        {countProduct}
-      </span>
+      <input
+        data-testid={ `customer_products__input-card-quantity-${product.id}` }
+        value={ countProduct }
+        onChange={ handleCoutProduct }
+        type="number"
+        min={ 0 }
+      />
       <button
-        data-testid={ `customer_products__button-card-add-item-${id}` }
+        data-testid={ `customer_products__button-card-add-item-${product.id}` }
+        className="btn-add"
         type="button"
-        onClick={ () => setCountProduct(countProduct + 1) }
+        onClick={ addProduct }
       >
         +
       </button>
@@ -28,7 +74,12 @@ function ButtonCardProduc({ id }) {
 }
 
 ButtonCardProduc.propTypes = {
-  id: PropTypes.number.isRequired,
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    price: PropTypes.string,
+    urlImage: PropTypes.string,
+  }).isRequired,
 };
 
 export default ButtonCardProduc;
