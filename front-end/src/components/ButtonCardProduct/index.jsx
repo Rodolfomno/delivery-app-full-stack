21deafca52e37aaae5761a9ea3b7d49aab0c69bb
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-06-08 13:47:04 -0300

    reafctor: passa useEffect para ButtonCard

--- front-end/src/components/ButtonCardProduct/index.jsx
+++ front-end/src/components/ButtonCardProduct/index.jsx
@@ -1,20 +1,25 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import MyContext from '../../context/MyContext';
 import './ButtonCardProduct.css';
 
 function ButtonCardProduc({ product }) {
   // const { id, name, price, urlImage } = product;
   const [countProduct, setCountProduct] = useState(0);
-
-  // const updateLocalStora = (boolean, cart) => {
-  //   if (boolean) {
-  //     const updateQtd = cart.map((prod) => ({ ...prod, qtd: countProduct + 1 }));
-  //     localStorage.setItem('cart', JSON.stringify(updateQtd));
-  //   } else {
-  //     cart.push({ ...product, qtd: countProduct + 1 });
-  //     localStorage.setItem('cart', JSON.stringify(cart));
-  //   }
-  // };
+  const { setCartTotalValue } = useContext(MyContext);
+  useEffect(() => {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    console.log('LS', cart);
+    if (cart.length) {
+      const totalValue = cart.map((prod) => Number(prod.price) * prod.qtd)
+        .reduce((acc, price) => acc + price, 0);
+      console.log('totalValueRDC', totalValue);
+      setCartTotalValue(totalValue
+        .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
+      console.log('DEptotalValueRDC', totalValue);
+      console.log('useEffect', cart);
+    }
+  }, [countProduct]);
 
   const removeProduct = () => {
     if (countProduct !== 0) {
@@ -28,9 +33,7 @@ function ButtonCardProduc({ product }) {
   const addProduct = () => {
     const cart = JSON.parse(localStorage.getItem('cart')) || [];
     setCountProduct(countProduct + 1);
-    console.log(cart);
     const ifContained = cart.some((prod) => prod.id === product.id);
-    // updateLocalStora(ifContained, cart);
     if (ifContained) {
       const updateQtd = cart.map((prod) => ({ ...prod, qtd: countProduct + 1 }));
       localStorage.setItem('cart', JSON.stringify(updateQtd));
