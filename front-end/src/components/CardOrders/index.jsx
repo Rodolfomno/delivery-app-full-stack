629a1fae71a097a855a731a4ff3068dda8ba432a
Walace <walacewab@gmail.com>
2022-06-08 17:47:10 -0300

    feat: adiciona os data-testid nos elementos

--- front-end/src/components/CardOrders/index.jsx
+++ front-end/src/components/CardOrders/index.jsx
@@ -7,15 +7,20 @@ function cardOrders() {
       <div className="card">
         <div className="request-number">
           <h3>Pedido</h3>
-          <h2>0001</h2>
+          <h2 data-testid="customer_orders__element-order-id-<id>">0001</h2>
         </div>
 
         <div className="request-status">
-          <h2>PREPARANDO</h2>
+          <h2 data-testid="customer_orders__element-delivery-status-<id>">PREPARANDO</h2>
         </div>
 
         <div className="request-dateprice">
-          <span className="card-dateprice">07/04/21</span>
+          <span
+            data-testid="customer_orders__element-order-date-<id>"
+            className="card-dateprice"
+          >
+            07/04/21
+          </span>
           <span className="card-dateprice">R$ 28,46</span>
         </div>
       </div>
