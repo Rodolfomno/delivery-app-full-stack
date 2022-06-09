1c916b1e07f3bb1a61271674e40b556ccf2b5b33
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-06-09 12:47:35 -0300

    style: checkout

--- front-end/src/pages/Checkout/index.jsx
+++ front-end/src/pages/Checkout/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import NavBar from '../../components/NavBar';
+import CardDetailsAndAddress from '../../components/CardDetailsAndAddress';
+import './Checkout.css';
 
 function Checkout() {
   return (
     <>
       <NavBar />
-      <h1>Checkout</h1>
+      <main>
+        <section className="orders-conteiner">
+          <h2>Finalizar Pedidos</h2>
+        </section>
+        <section className="delivery-container">
+          <h2>Detalhes e Endereço de Entrega</h2>
+          {/* <CardDetailsAndAddress /> */}
+        </section>
+      </main>
     </>
   );
 }
