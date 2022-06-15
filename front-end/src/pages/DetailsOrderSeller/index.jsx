4cd7270656cd3a1a80e41a456faae09c587bacca
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-06-15 17:35:58 -0300

    feat: page de pedidos e detalhes de seller

---
+++ front-end/src/pages/DetailsOrderSeller/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavbarSeller from '../../components/NavbarSeller';
+
+function DetailsOrderSeller() {
+  return (
+    <>
+      <NavbarSeller />
+      <h1>Detalhes do Pedido: SELLER</h1>
+    </>
+
+  );
+}
+
+export default DetailsOrderSeller;
