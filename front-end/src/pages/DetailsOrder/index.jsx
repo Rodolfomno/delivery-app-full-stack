2e80c56f489f35c5ab61b81101515cd0f1d4e402
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-06-20 17:25:49 -0300

    feat: request em orderDetailsCustomer

--- front-end/src/pages/DetailsOrder/index.jsx
+++ front-end/src/pages/DetailsOrder/index.jsx
@@ -1,127 +1,34 @@
-import React, { useState, useEffect } from 'react';
-//  import { useParams } from 'react-router-dom';
+import React, { useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import MyContext from '../../context/MyContext';
+import { requestData, setToken } from '../../service/request';
 import NavBar from '../../components/NavBar';
-import formatCurrency from '../../utils/formatCurrency';
-
-const dataTest = 'customer_order_details__element-order-details-label-delivery-status';
+import TableDetailsCustomer from '../../components/TableDetailsCustomer';
 
 function DetailsOrder() {
-  //  const { id } = useParams();
-  const [totalPrice, setTotalPrice] = useState(0);
-  const [cartItems, setCartItems] = useState([]);
-  //  const [isDelivered, setIsDelivered] = useState(false);
-
-  const mockDetails = {
-    id: '003',
-    nome: 'fulana pereira',
-    data: '06/06/1996',
-    entregue: 'entregue?',
-    marcar: 'marcarComo',
-  };
-
+  const { id: idParams } = useParams();
+  console.log('id', idParams);
+  const { setOrderDetails, orderDetails } = useContext(MyContext);
   useEffect(() => {
-    const cart = JSON.parse(localStorage.getItem('cart')) || [];
-    setCartItems(cart);
-  }, []);
+    const { token } = JSON.parse(localStorage.getItem('user')) || [];
+    (
+      async () => {
+        setToken(token);
+        const dataOrder = await requestData(`/sale/${idParams}`);
+        console.log('Order1', dataOrder);
 
-  useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('cart')) || [];
-    const total = data.reduce((acc, product) => acc + (product.qtd * product.price), 0);
-    setTotalPrice(formatCurrency(total));
-  }, [setTotalPrice]);
+        setOrderDetails(dataOrder[0]);
+      }
+    )();
+  }, [setOrderDetails, idParams]);
 
-  function handleButton() {
-    console.log('marca como entregue no banco');
-  }
   return (
     <>
       <NavBar />
       <h1>Detalhes do Pedido</h1>
-      <table border="1">
-        <tr>
-          <td
-            data-testid="customer_order_details__element-order-details-label-order-id"
-          >
-            pedido
-            {' '}
-            { mockDetails.id }
-          </td>
-          <td
-            data-testid="customer_order_details__element-order-details-label-seller-name"
-          >
-            P. vend:
-            {' '}
-            { mockDetails.nome }
-          </td>
-          <td
-            data-testid="customer_order_details__element-order-details-label-order-date"
-          >
-            { mockDetails.data }
-          </td>
-          <td
-            data-testid={ dataTest }
-          >
-            { mockDetails.entregue }
-
-          </td>
-          <button
-            data-testid="customer_order_details__button-delivery-check"
-            type="button"
-            name="marcarButton"
-            onClick={ handleButton }
-          >
-            Marcar como entregue
-          </button>
-        </tr>
-        <tr>
-          <td>Item</td>
-          <td>descrição</td>
-          <td>quantidade</td>
-          <td>Valor unitario</td>
-          <td>sub-total</td>
-        </tr>
-        { cartItems.map(({ name, price, qtd, id }, index) => (
-          <tr key={ index }>
-            <td
-              data-testid={
-                `customer_order_details__element-order-table-item-number-${index}`
-              }
-            >
-              { id }
-            </td>
-            <td
-              data-testid={
-                `customer_order_details__element-order-table-name-${index}`
-              }
-            >
-              { name }
-
-            </td>
-            <td
-              data-testid={
-                `customer_order_details__element-order-table-quantity-${index}`
-              }
-            >
-              { qtd }
-            </td>
-            <td
-              data-testid={
-                `customer_order_details__element-order-table-sub-total-${index}`
-              }
-            >
-              { formatCurrency(price) }
-            </td>
-            <td
-              data-testid={
-                `customer_order_details__element-order-total-price-${index}`
-              }
-            >
-              { formatCurrency(qtd * price) }
-            </td>
-          </tr>
-        )) }
-      </table>
-      <h2>{ totalPrice }</h2>
+      {
+        orderDetails && <TableDetailsCustomer />
+      }
     </>
 
   );
